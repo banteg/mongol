e16c85fd92c766272e1ca99f82f06d318ea61bc8
banteg <banteeg@gmail.com>
2014-10-13 04:24:52 +0700

    event streaming

--- mongol.py
+++ mongol.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+from bson import json_util
 import humanize
+from time import sleep
+import json
 
 app = Flask(__name__)
 app.debug = True
@@ -28,5 +31,23 @@ def event(id):
     return render_template('detail.html', log=log)
 
 
+def firehose():
+    total = db.find().count()
+    cursor = db.find(tailable=True).skip(total)
+    while cursor.alive:
+        try:
+            doc = next(cursor)
+            dump = json.dumps(doc, default=json_util.default)
+            yield 'data: {}\r\n\r\n'.format(dump)
+        except StopIteration:
+            sleep(1)
+
+
+@app.route('/api/stream')
+def stream():
+    return Response(firehose(),
+                    mimetype='text/event-stream')
+
+
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8000)
+    app.run(host='127.0.0.1', port=8000, threaded=True)
